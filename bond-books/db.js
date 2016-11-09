9e00dbbf811c7b7d7892a48801c5222576d22d4c
Yash Jalan <yashjalan@MacBook-Pro-2.local>
2016-11-08 19:01:09 -0500

    deploy

--- bond-books/db.js
+++ bond-books/db.js
@@ -1,21 +1,40 @@
 var mongoose = require('mongoose');
 var URLSlugs = require('mongoose-url-slugs');
 
-var User = new mongoose.Schema({
-  library:  [{ type: mongoose.Schema.Types.ObjectId, ref: 'Book' }]
-});
+//var User = new mongoose.Schema({
+//  library:  [{ type: mongoose.Schema.Types.ObjectId, ref: 'Book' }]
+//});
 
 var Book = new mongoose.Schema({
-  user: {type: mongoose.Schema.Types.ObjectId, ref:'User'},
+  //user: {type: mongoose.Schema.Types.ObjectId, ref:'User'},
   title: {type: String, required: true},
-  author: {type: String, required: false},
-  publisher: {type: String, required: false},
-  edition: {type: String, required: false},
-  discussion/comments: {type: [String], required: true},
-  image_url: {type: String, required: false},
-  createdAt: {type: Date, required: true},
-  genre: {type: String, required: true}
+  author: {type: String, required: true},
+  //publisher: {type: String, required: false},
+  //edition: {type: String, required: false},
+  //comments: {type: [String], required: false},
+  //image_url: {type: String, required: false},
+  //createdAt: {type: Date, required: false},
+  //genre: {type: String, required: false}
 });
 
+mongoose.model('Book', Book);
 
-mongoose.connect('mongodb://localhost/final-project');
+// is the environment variable, NODE_ENV, set to PRODUCTION? 
+if (process.env.NODE_ENV == 'PRODUCTION') {
+ // if we're in PRODUCTION mode, then read the configration from a file
+ // use blocking file io to do this...
+ var fs = require('fs');
+ var path = require('path');
+ var fn = path.join(__dirname, 'config.json');
+ var data = fs.readFileSync(fn);
+
+ // our configuration file will be in json, so parse it and set the
+ // conenction string appropriately!
+ var conf = JSON.parse(data);
+ var dbconf = conf.dbconf;
+} else {
+ // if we're not in PRODUCTION mode, then use
+ dbconf = 'mongodb://localhost/ait-final-project';
+}
+
+mongoose.connect(dbconf);
--- bond-books/routes/index.js
+++ bond-books/routes/index.js
@@ -1,9 +1,28 @@
 var express = require('express');
 var router = express.Router();
+require(__dirname + '/../db.js');
+var mongoose = require('mongoose');
+var Book = mongoose.model('Book');
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/books', function(req, res, next) {
+	var books = Book.find({}, function(err,books,count) {
+		if (!err) {
+			res.render('index.hbs', {books: books});
+		}
+	});
+
+});
+
+router.post('/books-add', function(req, res, next) {
+	new Book({
+		title: req.body.title,
+		author: req.body.author
+	}).save(function(err,book,count) {
+		if (!err) {
+			res.redirect('/books');
+		}
+	});
+
 });
 
 module.exports = router;
